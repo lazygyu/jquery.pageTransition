242d224b0d4a03784ddb0e1536aebde5028e5bc0
LazyGyu <lazygyu@gmail.com>
2016-03-21 23:11:30 +0900

    Update jquery.page-transition.js

--- jquery.page-transition.js
+++ jquery.page-transition.js
@@ -1,47 +1,60 @@
 (function($){
-				$.fn.pageTransition = function(opt){
-					var set = $.extend({
-							horizontal:40,
-							vertical:20,
-							max:1500,
-							bgColor:'black'
-						}, opt);
-					return this.each(function(){
-						
-						var tw = $(this).width();
-						var th = $(this).height();
+	$.pageTransition = function(opt){
+		var set = $.extend({
+				horizontal:40,
+				vertical:20,
+				max:1000,
+				bgColor:'black'
+			}, opt);
 
-						var w = tw/set.horizontal;
-						var h = th/set.vertical;
+		var blinder = $("<div></div>");
+		blinder.css({
+			position:'fixed',
+			top:0,
+			left:0,
+			right:0,
+			bottom:0,
+			background:set.bgColor,
+			zIndex:99999
+		});
+		blinder.appendTo("body");
+		
+		
+			
+			var tw = $(blinder).width();
+			var th = $(blinder).height();
 
-						var els = [];
+			var w = tw/set.horizontal;
+			var h = th/set.vertical;
 
-						var totalCnt = set.horizontal*set.vertical;
-						var hided = 0;
-						var that = this;
+			var els = [];
 
-						for(var i=0;i<totalCnt;i++){
-							els.push($("<span></span>"));
-							els[i].css({
-								backgroundColor:set.bgColor,
-								width:w + 'px',
-								height:h + 'px',
-								display:'block',
-								float:'left'
-							});
-							$(this).append(els[i]);
-							
-							setTimeout((function(v){
-								return function(){
-									els[v].css({backgroundColor:'transparent'});
-									hided++;
-									if( hided >= totalCnt ){
-										$(that).remove();
-									}
-								}
-							})(i), Math.floor(Math.random()*set.max));
+			var totalCnt = set.horizontal*set.vertical;
+			var hided = 0;
+			var that = blinder;
+
+			for(var i=0;i<totalCnt;i++){
+				els.push($("<span></span>"));
+				els[i].css({
+					backgroundColor:set.bgColor,
+					width:w + 'px',
+					height:h + 'px',
+					display:'block',
+					float:'left'
+				});
+				$(blinder).append(els[i]);
+				
+				setTimeout((function(v){
+					return function(){
+						els[v].css({backgroundColor:'transparent'});
+						hided++;
+						if( hided >= totalCnt ){
+							$(that).remove();
 						}
-						$(this).css({backgroundColor:'transparent'});
-					});
-				};
-			})(jQuery);
+					}
+				})(i), Math.floor(Math.random()*set.max));
+			}
+			$(blinder).css({backgroundColor:'transparent'});
+		
+	};
+})(jQuery);
